28256d6991952b6baa30d3039f81d75a7c8ced89
Anisberto Reis <anisbertoos@gmail.com>
2020-07-30 20:38:39 -0300

    Start Repositorio

---
+++ Aula_Data_27072020/exercicio/script/script.js
@@ -0,0 +1,34 @@
+var nPositivo = 15
+var nNegativo = -15
+var nDecimal = 2.3
+var booleano = true
+var nome = "Anisberto"
+var sobreNome = "Reis"
+
+console.log(nome + " " + sobreNome)
+var valorNulo = null
+var naoDefinido = undefined
+var mes = 12
+
+switch (mes) {
+    case 1:
+        'anisberto'
+        break
+    case 2:
+        'Anisberto'
+        break
+}
+
+console.log(valorNulo)
+console.log(naoDefinido)
+
+var dh = "Digital House, a escola de programação que forma novas gerações de coders e profissionais digitais para imaginar, inovar e criar o que sempre sonharam."
+console.log(dh)
+
+var mat = new mat[2][2]
+
+for (var i = 0; i < 2; i++) {
+    for (var j = 0; j < 2; j++) {
+        console.log(mat[i][j])
+    }
+}
