0082c65b121b448f2a915666252e53636abb61f2
Anisberto Reis <anisbertoos@gmail.com>
2020-08-02 01:18:59 -0300

    Aula eventos

---
+++ Aula_Data_31072020/Exercicio/script/script.js
@@ -0,0 +1,14 @@
+function cumprimentar() {
+    alert("Ola!")
+}
+
+function alterar_fundo() {
+    var fundo = document.getElementById('body');
+    fundo.style.backgroundColor = "green";
+    document.getElementById('cont-controle').style.borderColor = "green"
+}
+
+function boasVindas() {
+    alert('estou vendo a imagem')
+    console.log('estou vendo a imagem')
+}
